720e44b9e8308259816721dfc721f54e451a321e
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-23 15:37:59 +0530

    'module.greeting'

---
+++ a.py
@@ -0,0 +1,7 @@
+#1.Function defination is one time process
+def greeting(name):
+    print("Hello, " + name)
+    
+
+
+
---
+++ b.py
@@ -0,0 +1,7 @@
+
+#import moduleName
+import a
+
+#2. Function calling/invoking is many time process
+#moduleName.functionName()
+a.greeting("Pranav")
